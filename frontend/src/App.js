eaf56d403869b210f3eb9f092f631934ee92b9a3
Clenio <clenio.si@gmail.com>
2019-05-01 23:09:30 -0300

    finalizando frontend com upload de arquivos usando o react dropzone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './App.css';
-import Main from './pages/Main';
+import Routes from './routes';
 
-function App() {
-  return (
-    <Main />
-  );
-}
+const App = () => <Routes />;
 
 export default App;
