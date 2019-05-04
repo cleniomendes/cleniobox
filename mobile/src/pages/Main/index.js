c5fe48c3e67c0de0c51c5916ec709f40833dd43a
Clenio <clenio.si@gmail.com>
2019-05-04 02:29:35 -0300

    inicio aplicacao mobile

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import styles from './styles';
+import logo from '../../assets/logo.png';
+
+export default class Main extends Component {
+  render() {
+    return <View>
+                <Text>Olá mundo!</Text>
+            </View>;
+  }
+}
